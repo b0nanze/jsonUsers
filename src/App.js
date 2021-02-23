b62c2b170f35b4874e13b9fc82530004f9d7d1a8
b0nanze <iumkhaev@gmail.com>
2021-02-23 21:29:45 +0300

    f

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import {useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(json => {
+                setData(json)
+                setLoading(false)
+            })
+    },[])
+
+    return (
+      <div className="container">
+          <div>
+            <h1>Users:</h1>
+          </div>
+              {loading ? (
+                  <div className="spinner-border text-danger" role="status">
+                      <span className="visually-hidden">Загрузка...</span>
+                  </div>
+              ) : data.map(item =>{
+                  return(
+                  <div>
+                      <div className="list-group">
+                          <a href="#" className="list-group-item list-group-item-action mb-3 row">
+                              <h2>Name: {item.username}</h2>
+                              <div>
+                                  Email: {item.email}
+                              </div>
+                              <div>
+                                  City: {item.address.city}
+                              </div>
+                              <div>
+                                  Phone: {item.phone}
+                              </div>
+                              <div>
+                                  Company: {item.company.name}
+                              </div>
+                          </a>
+                      </div>
+                  </div>
+                  )
+              })}
+      </div>
   );
 }
 
